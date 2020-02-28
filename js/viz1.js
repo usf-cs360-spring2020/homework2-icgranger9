5c81616c7833b0a76cf838131b4910276bf72dd5
Ian Granger <icgranger9@gmail.com>
2020-02-27 16:47:41 -0800

    Got lines, need to fix sizing

--- js/viz1.js
+++ js/viz1.js
@@ -1,14 +1,19 @@
+let translate = function(x, y) {
+	return 'translate(' + x + ',' + y + ')';
+}
+
 let createParallelCoords = function(file){
 
 	// Selects only the relevant fields to save
 	d3.csv(file, function(d) {
 		return {
-			"tier": +d["tier"],
-			"tier_name": d["tier_name"],
-			"k_rank": +d["k_rank"],
-			"k_mean": +d["k_mean"],
 			"par_rank": +d["par_rank"],
 			"par_mean": +d["par_mean"],
+			"tier": +d["tier"],
+			"k_mean": +d["k_mean"],
+			"k_rank": +d["k_rank"],
+
+			"tier_name": d["tier_name"], // Not sure we need it, but leaving for now
 		};
 
 	}).then(function(data) {
@@ -22,6 +27,18 @@ let createParallelCoords = function(file){
 let drawEverything = function(data) {
 
 	// First, let's create some constants for use latter 
+	
+	const fields = Object.keys(data[0]).filter( item => item != "tier_name");
+
+	const tiers = ["Ivy Plus", "Other elite schools (public and private)", "Highly selective public", 
+	"Highly selective private", "Selective public", "Selective private", "Nonselective 4-year public", 
+	"Nonselective 4-year private not-for-profit", "Two-year (public and private not-for-profit)", 
+	"Four-year for-profit", "Two-year for-profit", "Less than two year schools of any type",
+	 "Attending college with insufficient data", "Not in college between the ages of 19-22"]
+
+	 const width = 960;
+	const height = 500;
+
 	const margin = {
 		top:	30,
 		right:	35, 
@@ -29,202 +46,68 @@ let drawEverything = function(data) {
 		left:	75	// leave space for y-axis
 	};
 	
-	// get the svg to draw on
-	let svg = d3.select("body").select("svg#viz");
+	const svg = d3.select("body").select("svg#viz")
+		.attr("width", width)
+		.attr("height", height);
+
 	console.assert(svg.size() == 1);
 
 
 	// Now, let's call the key functions that actually do everything 
-	drawAxes()
-	drawLines()
-	drawLegend()
+	drawLines(data);
+	drawAxes();
+	drawLegend();
 
 	// Finally, we need to actually implement those functions
+	function drawLines (data) {
+		console.log("In draw lines")
+
+		let yAxis = {} // What's a better name than y?
+		for (curr_filed of fields) {
+			let curr_min = d3.min(data, d => d[curr_filed])
+			let curr_max = d3.max(data, d => d[curr_filed])
+
+			yAxis[curr_filed] = d3.scaleLinear()
+				.domain([curr_min, curr_max])
+				.range([height, 0])
+
+			console.log(curr_filed);
+		}
+
+		console.log("width: ", width)
+
+		let xAxis = d3.scalePoint()
+			.range([0, width]) // This is throwing off the width for some reason, not sure why
+			.padding(.5)
+			.domain(fields);
+
+		// Note :Might need plot later. Should probably make it a constant
+		let plot = svg.append("g").attr("id", "plot");
+		// plot.attr("transform", translate(margin.left, margin.top));
+
+		let lines = plot.selectAll("line")
+			.data(data);
+
+		lines.enter().append("path")
+			.attr("d",  function(d) {
+				return d3.line()(fields.map(function(p) {
+				return [xAxis(p), yAxis[p](d[p])];
+			}))})
+			.style("fill", "none")
+			.style("stroke", "#5F42D6") // Need to add color
+			.style("opacity", 0.25);
+
+		debugger;
+	}
 
 	function drawAxes() {
 		console.log("In draw axes")
-	}
 
-	function drawLines () {
-		console.log("In draw lines")
 	}
 
 	function drawLegend () {
 		console.log("In draw legend")
 	}
-
-	// Draw axes
-	//calculate the min and max of our data
-	let countMin = 0;
-	let countMax = d3.max(data, function(d) { return d.domestic +  d.international});
-
-	let periodMin = d3.min(data, function(d) { return d.period;})
-	let periodMax = d3.max(data, function(d) { return d.period;})
-
-	// Should never hit this
-	if (isNaN(countMax)) {
-		countMax = 0;
-	}
-
-	// now we can calculate how much space we have to plot
-	let bounds = svg.node().getBoundingClientRect();
-	let plotWidth = bounds.width - margin.right - margin.left;
-	let plotHeight = bounds.height - margin.top - margin.bottom;
-
-
-	//y Scale
-	let countScale = d3.scaleLinear()
-		.domain([countMin, countMax])
-		.range([plotHeight, 0])
-		.nice();
-
-	// x scale
-	let periodScale = d3.scaleBand()
-		.domain(data.map(d => d.period))
-		.rangeRound([0, plotWidth])
-		.paddingInner(0.01); // space between bars
-
-	// z scale
-	let colorKey = ["domestic", "international"]
-	let colorScale = d3.scaleOrdinal()
-		.range(["#5779a3", "#85b5b3"])
-		.domain(colorKey);
-
-	// create the plot
-	let plot = svg.append("g").attr("id", "plot");
-	plot.attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-	console.assert(plot.size() == 1);
-
-	// now lets draw our x-axis
-	let xAxis = d3.axisBottom(periodScale);
-	xAxis.tickFormat(function(d, i){
-		return ["January", "February", "March", "April", "May", "June", "July", "August", "September"][i]
-	})
-
-	let xGroup = plot.append("g").attr("id", "x-axis");
-	xGroup.call(xAxis);
-	xGroup.attr("transform", "translate(0," + plotHeight + ")");
-
-	// do the same for our y axis
-	let yAxis = d3.axisLeft(countScale);
-	yAxis.tickFormat(d3.formatPrefix(".0", 1e6));
-	yAxis.ticks(7);
-	let yGroup = plot.append("g").attr("id", "y-axis");
-	yGroup.call(yAxis);
-
-
-	//Draw titles
-	const xTitle = xGroup.append('text')
-    	.attr('class', 'axis-title')
-    	.text('Activity Period for [2019]');
-
-	xTitle.attr('x', 0);
-	xTitle.attr('y', 0);
-	xTitle.attr('dy', 45);
-	xTitle.attr('dx', 400);
-	xTitle.attr('text-anchor', 'middle');
-
-
-	const yTitle = yGroup.append('text')
-		.attr('class', 'axis-title')
-		.text('Passenger Count');
-
-	// keep x, y at 0, 0 for rotation around the origin
-	yTitle.attr('x', 0);
-	yTitle.attr('y', 0);
-	yTitle.attr('dy', -50);
-	yTitle.attr('dx', -200);
-	yTitle.attr('text-anchor', 'middle');
-	yTitle.attr('transform', 'rotate(-90)');
-	
-	// Draw Bars
-
-	var group = plot.selectAll("g.layer")
-			.data(d3.stack().keys(colorKey)(data), function(d) {d.period});
-
-      group.exit().remove()
-
-      group.enter().append("g")
-			.classed("layer", true)
-			.attr("fill", d => colorScale(d.key));
-
-	let bars = plot.selectAll("g.layer").selectAll("rect")
-		.data(data, d=>d);
-
-	// I know this is a bad way to do it, but I can't think of anything else. 
-	// I will ask you about it after class.
-	var yCount = 0;
-	var hCount = 0;
-
-	// we use the enter() selection to add new bars for new data
-	bars.enter().append("rect")
-		.attr("class", "bar")
-		.attr("width", periodScale.bandwidth())
-		.attr("x", d => periodScale(d.period))
-		.attr("y", function(d,i) {
-			if (yCount < 9){
-				yCount +=1;
-				return countScale(d.domestic + d.international) - 1 // -1 to give a 1px gap
-
-			}
-			else {
-				return countScale(d.international)
-			}
-		})
-		.attr("height",function(d,i) {
-			if (hCount < 9){
-				hCount +=1;
-				return plotHeight - countScale(d.domestic)
-			}
-			else {
-				return plotHeight - countScale(d.international)
-			}
-		});
-
 };
-
-
-let createLegend = function() {
-	const margin = {
-		top:	30,
-		right:	35, 
-		bottom: 60, // leave space for x-axis
-		left:	75	// leave space for y-axis
-	};
-
-	let svg = d3.select("body").select("svg#legend");
-
-	let colorKey = ["Domestic", "International"]
-	let colorScale = d3.scaleOrdinal()
-		.range(["#5779a3", "#85b5b3"])
-		.domain(colorKey);
-
-	let squares = svg.selectAll("squares")
-		.data(colorKey);
-
-	squares.enter().append("rect")
-		.attr("class", "legend-squares")
-		.attr("x", 0)
-		.attr("y", function(d,i){ return 30 + i*30})
-		.attr("width", 15)
-		.attr("height", 15)
-		.attr("fill", d => colorScale(d))
-
-	let labels = svg.selectAll("squares")
-		.data(colorKey);
-
-	labels.enter().append("text")
-		.attr("class", "legend-labels")
-		.attr("x", 20)
-		.attr("y", function(d,i){ return 45 + i*30})
-		.text(function(d){ return d})
-
-	svg.append("text")
-		.attr("class","legend-title")
-		.attr("x", 0)
-		.attr("y", 20)
-		.text("Passenger Type");
-}
 
 
