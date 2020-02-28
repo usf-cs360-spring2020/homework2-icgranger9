d1f8ef932fd2708e6425a0946171495482f17313
Ian Granger <icgranger9@gmail.com>
2020-02-28 11:40:13 -0800

    Viz pretty much done, need to update prototype and site info

--- js/viz1.js
+++ js/viz1.js
@@ -1,20 +1,19 @@
-let translate = function(x, y) {
-	return 'translate(' + x + ',' + y + ')';
-}
-
 let createParallelCoords = function(file){
 
 	// Selects only the relevant fields to save
 	d3.csv(file, function(d) {
-		return {
+		let res = {
 			"par_rank": +d["par_rank"],
 			"par_mean": +d["par_mean"],
-			"tier": +d["tier"],
+			// "tier": +d["tier"],
 			"k_mean": +d["k_mean"],
 			"k_rank": +d["k_rank"],
-
-			"tier_name": d["tier_name"], // Not sure we need it, but leaving for now
 		};
+
+		// Groups tiers, for color scale
+		res["tier_group"] = group_tier(d["tier_name"])
+
+		return res
 
 	}).then(function(data) {
 
@@ -28,30 +27,56 @@ let drawEverything = function(data) {
 
 	// First, let's create some constants for use latter 
 	
-	const fields = Object.keys(data[0]).filter( item => item != "tier_name");
+	const fields = Object.keys(data[0]).filter( item => item != "tier_group");
 
-	const tiers = ["Ivy Plus", "Other elite schools (public and private)", "Highly selective public", 
-	"Highly selective private", "Selective public", "Selective private", "Nonselective 4-year public", 
-	"Nonselective 4-year private not-for-profit", "Two-year (public and private not-for-profit)", 
-	"Four-year for-profit", "Two-year for-profit", "Less than two year schools of any type",
-	 "Attending college with insufficient data", "Not in college between the ages of 19-22"]
+	const tiers = ["Other", "Two Year or Less", "For Profit", "Non-selective", "Selective", "Highly Selective", "Elite"]
 
-	 const width = 960;
+	const width = 1200; //960 for plot, 200 for legend, 40 gap between
 	const height = 500;
 
 	const margin = {
-		top:	30,
-		right:	35, 
-		bottom: 60, // leave space for x-axis
-		left:	75	// leave space for y-axis
+		top:	40,
+		right:	280, // 240ish for legend, plus a gap between the two
+		bottom: 20,
+		left:	40
 	};
-	
+
+	const plotWidth = width - margin.right - margin.left;
+	const plotHeight = height - margin.top - margin.bottom;
+
+
 	const svg = d3.select("body").select("svg#viz")
 		.attr("width", width)
 		.attr("height", height);
 
 	console.assert(svg.size() == 1);
 
+	const plot = svg.append("g").attr("id", "plot")
+		.attr("transform", translate(margin.left, margin.top));
+
+	// Axes can't be in a specific function, because they're used by all / multiple.
+
+	// Need a different scale for each field
+	const yAxis = {}
+		for (curr_filed of fields) {
+			let curr_min = d3.min(data, d => d[curr_filed])
+			let curr_max = d3.max(data, d => d[curr_filed])
+
+			yAxis[curr_filed] = d3.scaleLinear()
+				.domain([curr_min, curr_max])
+				.range([plotHeight, 0])
+
+		}
+
+	// Only need one x-axis
+	const xAxis = d3.scalePoint()
+		.range([0, plotWidth])
+		.domain(fields);
+
+	// Needs a better Name
+	const colorScale = d3.scaleOrdinal()
+		.range(d3.schemeYlGnBu[7])
+		.domain(tiers);
 
 	// Now, let's call the key functions that actually do everything 
 	drawLines(data);
@@ -62,28 +87,6 @@ let drawEverything = function(data) {
 	function drawLines (data) {
 		console.log("In draw lines")
 
-		let yAxis = {} // What's a better name than y?
-		for (curr_filed of fields) {
-			let curr_min = d3.min(data, d => d[curr_filed])
-			let curr_max = d3.max(data, d => d[curr_filed])
-
-			yAxis[curr_filed] = d3.scaleLinear()
-				.domain([curr_min, curr_max])
-				.range([height, 0])
-
-			console.log(curr_filed);
-		}
-
-		console.log("width: ", width)
-
-		let xAxis = d3.scalePoint()
-			.range([0, width]) // This is throwing off the width for some reason, not sure why
-			.padding(.5)
-			.domain(fields);
-
-		// Note :Might need plot later. Should probably make it a constant
-		let plot = svg.append("g").attr("id", "plot");
-		// plot.attr("transform", translate(margin.left, margin.top));
 
 		let lines = plot.selectAll("line")
 			.data(data);
@@ -94,20 +97,127 @@ let drawEverything = function(data) {
 				return [xAxis(p), yAxis[p](d[p])];
 			}))})
 			.style("fill", "none")
-			.style("stroke", "#5F42D6") // Need to add color
+			.style("stroke", d => colorScale(d.tier_group))
 			.style("opacity", 0.25);
 
-		debugger;
 	}
 
 	function drawAxes() {
 		console.log("In draw axes")
 
+		let axes = plot.selectAll("x-axis")
+			.data(fields);
+
+		axes.enter().append("g")
+			.attr("transform",  d=> translate(xAxis(d), 0))
+			.each(function(d) {
+				d3.select(this).call(d3.axisLeft().scale(yAxis[d]));
+			});
+
+
+		axes.enter().append("text")
+			.style("text-anchor", "middle")
+			.attr("transform",  d=> translate(xAxis(d), 0-(margin.top/2)))
+			.attr("y", -1)
+			.text(d => d);
+
 	}
 
 	function drawLegend () {
 		console.log("In draw legend")
+
+		// Needed to position the legend
+		let legendX = width - margin.right + 40
+
+		// Must make a scale, bot the distance between each legend item
+		const legendScale = d3.scalePoint()
+			.range([plotHeight/3, 0])
+			.domain(tiers);
+
+		let legend = svg.append("g").attr("id", "legend")
+			.attr("transform", translate(legendX, 20));
+
+
+		// legend title
+		legend.append("text")
+			.attr("class","legend-title")
+			.attr("x", 0)
+			.attr("y", 0)
+			.text("College Tier");
+
+		let legendItem = legend.selectAll("x-axis")
+			.data(tiers);
+
+
+		// Add A colored rect for each item
+		legendItem.enter().append("rect")
+			.attr("class", "legend-squares")
+			.attr("transform", d=> translate(0, legendScale(d)+8))
+			.attr("width", 15)
+			.attr("height", 15)
+			.attr("fill", d => colorScale(d))
+
+		// Label each item in the legend
+		legendItem.enter().append("text")
+			.attr("class", "legend-labels")
+			.attr("transform", d=> translate(25, legendScale(d)+20))
+			.text(d => d);
 	}
 };
+
+
+//Helper functions, to make life easier
+let translate = function(x, y) {
+	return 'translate(' + x + ',' + y + ')';
+}
+
+let group_tier = function(tier_name) {
+	let res = ""
+
+	switch(tier_name) {
+
+		case "Ivy Plus":
+		case "Other elite schools (public and private)":
+			res = "elite";
+			break;
+
+		case "Highly selective public":
+		case "Highly selective private":
+			res = "highly_selective";
+			break;
+
+		case "Selective public":
+		case "Selective private":
+			res = "selective"
+			break;
+
+		case "Nonselective 4-year public":
+		case "Nonselective 4-year private not-for-profit":
+			res = "nonselective";
+			break;
+
+		case "Four-year for-profit":
+		case "Two-year for-profit":
+			res = "for_profit";
+			break;
+
+		case "Two-year (public and private not-for-profit)":
+		case "Less than two year schools of any type":
+			res = "two_year_or_less";
+			break;
+
+		case "Attending college with insufficient data":
+		case "Not in college between the ages of 19-22":
+			res = "other"; // Should probably have a better name for this
+			break;
+
+		default:
+			console.log(`Got invalid tier name, something is very wrong: ${tier_name} `)
+	}
+
+	return res;
+}
+
+
 
 
